75e2d4000537899a652b8242ea9b9f5b4302c275
gollaswati <swathinaidugolla@example.com>
2022-01-04 07:04:02 +0530

    chnges

---
+++ link.js
@@ -0,0 +1,32 @@
+var a = "name";
+if (a === "name") {
+  alert("my name is swathi");
+}
+console.log(40 + 10 + 10 - 5);
+let firstName = "swathi";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myName = "swathiiiiiiiiiiiiii";
+console.log(myName);
+let isthis = true;
+console.log(isthis);
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "name");
+console.log(typeof false);
+console.log(typeof javascriptIsfun);
+
+let myfirstjob = "software engineer";
+let secondsalary = 21000;
+console.log(secondsalary);
+
+year = 1999;
+console.log(typeof year);
+
+console.log(typeof null);
+
+myName = "golla swathi";
+console.log(typeof myName);
