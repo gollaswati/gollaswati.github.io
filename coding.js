d4ded4ff1d595958abb5f97575544fdd08fa3a4e
gollaswati <swathinaidugolla@example.com>
2022-01-06 06:59:31 +0530

    chnges

---
+++ coding.js
@@ -0,0 +1,128 @@
+// const massMark = 20;
+// const heightMark = 10.5;
+// const massJone = 30;
+// const heighjone = 10.4;
+
+// const NMImark = massMark / heightMark ** 2;
+// const BMImark = massJone / (heighjone * heighjone);
+// const bmiHighermark = NMImark > BMImark;
+// console.log(bmiHighermark);
+// console.log(NMImark);
+// console.log(BMImark);
+
+// if (NMImark > BMImark) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// const firstname = "swathi";
+// const job = "softwate";
+// const birthyear = 1999;
+// const year = 1995;
+
+// const jones = "i'm " + firstname + ",a " + (year - birthyear);
+// console.log(jones);
+
+// const jonesnew = `iam ${firstname},a ${jones}`;
+// console.log(jonesnew);
+
+// console.log("im swahi");
+// console.log("im swathi \n from madhnapali \n working in volkoteck");
+
+// const age = 19;
+// const isOld = age >= 16;
+
+// if (isOld) {
+//   console.log("swathi can drive");
+// }
+
+// const age = 10;
+// const isOld = age >= 16;
+
+// if (isOld) {
+//   console.log("swathi can drive");
+// }
+
+// const age = 18;
+
+// if (age >= 19) {
+//   console.log("can drive");
+// } else {
+//   console.log("can't drive");
+// }
+
+// const birthyear = 2012;
+// let country;
+// if (birthyear <= 2000) {
+//   country = 20;
+// } else {
+//   country = 21;
+// }
+// console.log(country);
+
+//type of covertion
+// const inputyear = 1999;
+// console.log(Number(inputyear));
+// console.log(inputyear + 18);
+
+// console.log(Number("jones"));
+
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+//type of coersion
+// console.log("im " + "swathi " - 23 + "old");
+// console.log("29" + "10" + 9);
+// console.log("29" - "10" + 9);
+
+// let n = "1" + 1; //11
+// n = n - 1; //10
+// console.log(n);
+
+// 5 false values:0,null,nan,undefined,''
+// console.log(Boolean(0));
+// console.log(Boolean("jones"));
+// console.log(Boolean(undefined));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 0;
+// if (money) {
+//   console.log("spend it all money");
+// } else {
+//   console.log("dont spend money");
+// }
+
+// let height;
+// if (height) {
+//   console.log("big height");
+// } else {
+//   console.log("height undefined");
+// }
+
+// let weight = 10;
+// if (weight) {
+//   console.log("big height");
+// } else {
+//   console.log("height undefined");
+// }
+
+const age = 18;
+
+if (age === 18) console.log("your just become adult (strict)");
+
+if (age == 18) console.log("your just become adult (loose)");
+
+const favorite = prompt("whats your favorite number");
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 25) {
+  console.log("good");
+} else if (favorite === 7) {
+  console.log("bad");
+} else {
+  console.log("think");
+}
--- link.js
+++ link.js
@@ -1,32 +1,97 @@
-var a = "name";
-if (a === "name") {
-  alert("my name is swathi");
-}
-console.log(40 + 10 + 10 - 5);
-let firstName = "swathi";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// var a = "name";
+// if (a === "name") {
+//   alert("my name is swathi");
+// }
+// console.log(40 + 10 + 10 - 5);
+// let firstName = "swathi";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let myName = "swathiiiiiiiiiiiiii";
-console.log(myName);
-let isthis = true;
-console.log(isthis);
-console.log(typeof true);
-console.log(typeof 23);
-console.log(typeof "name");
-console.log(typeof false);
-console.log(typeof javascriptIsfun);
+// let myName = "swathiiiiiiiiiiiiii";
+// console.log(myName);
+// let isthis = true;
+// console.log(isthis);
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof "name");
+// console.log(typeof false);
+// console.log(typeof javascriptIsfun);
 
-let myfirstjob = "software engineer";
-let secondsalary = 21000;
-console.log(secondsalary);
+// let myfirstjob = "software engineer";
+// let secondsalary = 21000;
+// console.log(secondsalary);
 
-year = 1999;
-console.log(typeof year);
+// year = 1999;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
 
-myName = "golla swathi";
-console.log(typeof myName);
+// myName = "golla swathi";
+// console.log(typeof myName);
+
+// let photo = "flower";
+// photo = "flowers";
+
+// const pic = "flower";
+// pic = "flowers";
+// var think = "idea";
+// think = "ideas";
+
+// firstName = "swathi";
+// console.log(firstName);
+
+// const sub = 200 - 100;
+// const sub1 = 200 - 100;
+// console.log(sub, sub1);
+// math opertator
+// const sub2 = 200;
+// const sub = sub2 - 100;
+// const sub1 = sub2 - 100;
+// console.log(sub, sub1);
+
+// console.log(sub2 - 100, 2 ** 3, sub + 100);
+
+// const firsname = "good";
+// const lasname = "to go";
+// console.log(firsname + lasname);
+// assignment operator
+// let x = 4 + 10;
+// x += 10;
+// x += 3;
+// x++;
+// x--;
+// x *= 10;
+// console.log(x);
+// comparision operator
+
+// const flower = 100;
+// const flowers = 120;
+// console.log(flower >= flowers);
+
+// console.log(100 < 120);
+
+// const now = 2037;
+// const ageswathi = now - 1999;
+// const agegolla = now - 2019;
+// console.log(now - 1999 > now - 2019);
+
+let x, y;
+x = y = 34 - 10 - 4;
+console.log(x, y);
+
+const averageage = ageswathi + agegolla / 2;
+console.log(ageswathi, agegolla, averageage);
+
+const averageage = (ageswathi + agegolla) / 2;
+console.log(ageswathi, agegolla, averageage);
+
+const averageage = ageswathi - agegolla / 2;
+console.log(ageswathi, agegolla, averageage);
+
+const averageage = (ageswathi - agegolla) / 2;
+console.log(ageswathi, agegolla, averageage);
+
+const averageage = (ageswathi * agegolla) / 2;
+console.log(ageswathi, agegolla, averageage);
