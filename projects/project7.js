7271ec78fbe7071e2c6869305b641ce663d262d8
gollaswati <swathinaidugolla@example.com>
2022-03-15 07:15:45 +0530

    changes

--- projects/project7.js
+++ projects/project7.js
@@ -1,17 +1,30 @@
-const container = document.querySelector(".container");
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
+// const container = document.querySelector(".container");
+// const left = document.querySelector(".left");
+// const right = document.querySelector(".right");
 
-left.addEventListener("mouseenter", () =>
-  container.classList.add("hover-left")
-);
-left.addEventListener("mouseleave", () =>
-  container.classList.remove("hover-left")
-);
+// left.addEventListener("mouseenter", () =>
+//   container.classList.add("hover-left")
+// );
+// left.addEventListener("mouseleave", () =>
+//   container.classList.remove("hover-left")
+// );
 
-right.addEventListener("mouseenter", () =>
-  container.classList.add("hover-right")
-);
-right.addEventListener("mouseleave", () =>
-  container.classList.remove("hover-right")
-);
+// right.addEventListener("mouseenter", () =>
+//   container.classList.add("hover-right")
+// );
+// right.addEventListener("mouseleave", () =>
+//   container.classList.remove("hover-right")
+// );
+
+$(".left").mouseenter(function () {
+  $(".container").addClass("hover-left");
+});
+$(".left").mouseleave(function () {
+  $(".container").removeClass("hover-left");
+});
+$(".right").mouseenter(function () {
+  $(".container").addClass("hover-right");
+});
+$(".right").mouseleave(function () {
+  $(".container").removeClass("hover-right");
+});
